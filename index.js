4a2430450ea39dfa3afbb0103ac332b907c5efb1
prakashMali1527 <prakash.dakshana@gmail.com>
2024-05-25 08:43:46 +0530

    Upload file using AJAX xhr Request

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const dropZone = document.querySelector('.drop-zone');
 const fileInput = document.querySelector('#file-input');
 const browseBtn = document.querySelector('.browse-btn');
 
+// change host and upload url
+// const host = 'https://innshare.herokuapp.com/';
+const host = '';
+const uploadUrl = `${host}api/files`;
+
 // transform icon on dragging or droping
 dropZone.addEventListener("dragover", (e) => {
     e.preventDefault();
@@ -16,10 +21,38 @@ dropZone.addEventListener('drop', (e) => {
     e.preventDefault();
     dropZone.classList.remove('dragged');
     const files = e.dataTransfer.files;
-    if(files.length > 0)
+    if(files.length > 0){
         fileInput.files = files;
-})
+        uploadFile();
+    }
+});
+
+// fileInput.addEventListener('change',()=>{
+//     uploadFile();
+// });
 
 browseBtn.addEventListener('click', (e) => {
     fileInput.click();
 })
+
+const uploadFile = () => {
+
+    const file = fileInput.files[0];
+
+    const formData = new FormData();
+    formData.append('myfile',file);
+
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        console.log(xhr.readyState);
+        if(xhr.readyState == XMLHttpRequest.DONE){
+            console.log('package received');
+            console.log(xhr.response);
+        }
+    }
+
+    xhr.open('POST', uploadUrl);
+    xhr.send(formData);
+}
+
+fileInput.addEventListener('change', uploadFile);
